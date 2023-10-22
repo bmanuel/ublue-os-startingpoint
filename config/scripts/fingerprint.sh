9f5bb16741d37e286f8e567c138f21b0fc2f8c68
Benjamin Manuel <ben@benmanuel.com>
2023-10-22 12:00:03 -0500

    Enable fingerprint services by default

--- config/scripts/fingerprint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-rpm-ostree override remove fprintd fprintd-pam \
-	--install open-fprintd \
-	--install python3-validity \
-	--install fprintd-clients \
-	--install fprintd-clients-pam
